8530da44f9815327729ec8cfcd37b345871ee931
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-09-13 14:14:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function threeSum(arr, target) {
 // write your code here
 	let obj={};
 	for(let i=0;i<arr.length;i++){
-		if(obj(arr[i])!= undefined){
-			obj(arr[i]) +=1;
+		if(obj[arr[i]]!= undefined){
+			obj[arr[i]] +=1;
 		}
 		else{
-			obj(arr[i])=1;
+			obj[arr[i]]=1;
 		}
 	}
 	for(let i=0;i<arr.length;i++){
-		if(obj(arr[i])==1){
+		if(obj[arr[i]]==1){
 			return arr[i];
 		}
 	}
